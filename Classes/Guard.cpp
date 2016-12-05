f5f6c4586a38c7fae59ebe263f1b0a288279999f
pizishusheng <codingmylife@yeah.net>
2016-12-05 18:44:22 +0800

    新加入关卡守卫数据模型和关卡配置文件

---
+++ Classes/Guard.cpp
@@ -0,0 +1,9 @@
+//
+//  Guard.cpp
+//  JFDS
+//
+//  Created by Tao on 2016/12/5.
+//
+//
+
+#include "Guard.hpp"
---
+++ Classes/Guard.hpp
@@ -0,0 +1,23 @@
+//
+//  Guard.hpp
+//  JFDS
+//
+//  Created by Tao on 2016/12/5.
+//
+//
+
+#ifndef Guard_hpp
+#define Guard_hpp
+
+#include <stdio.h>
+
+class Guard
+{
+public:
+private:
+    int m_PositionX;
+    int m_PositionY;
+    
+};
+
+#endif /* Guard_hpp */
